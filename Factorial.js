19940802b618ddc5bea258db3a08e6f43b2e444e
kremlevmax <kremlevmax.webdev@icloud.com>
2023-04-24 13:14:26 -0700

    Factoriall

---
+++ Factorial.js
@@ -0,0 +1,22 @@
+// Factorial
+
+/*
+DESCRIPTION:
+Your task is to write function factorial.
+
+https://en.wikipedia.org/wiki/Factorial
+*/
+
+function factorial(n){
+ let result = n;
+  
+  if (n === 0 || n === 1) 
+    return 1; 
+  
+  while (n > 1) { 
+    n--;
+    result *= n;
+  }
+  
+  return result;
+}
